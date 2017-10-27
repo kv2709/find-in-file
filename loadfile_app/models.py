a6f707855d80154aa5a5068a6b9aa315fc163d44
kv2709 <kv2709$gmail.com>
2017-10-27 10:12:07 +0600

    Add media folder

--- loadfile_app/models.py
+++ loadfile_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class LoadFile(models.Model):
     """Set field loadfile"""
     file_name = models.CharField(max_length=200, blank=True)
-    file_obj = models.FileField(upload_to='media')
+    file_obj = models.FileField()
     str_for_search = models.CharField(max_length=200)
     file_size = models.IntegerField()
     count_found = models.SmallIntegerField()
